8fa77a0c324549fb08fff7b9d81a06657ceb8f1d
Vanessa8300 <73070067+Vanessa8300@users.noreply.github.com>
2020-11-13 17:22:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+var intrebari= [
+    {
+        prompt: "Ce este metafora? \n (a)Metafora este o figură de stil de nivel lexico-semantic care constă în folosirea unui termen impropriu, dar plastic, evocator sau sugestiv, în locul termenului propriu.\n (b) Metafora este o figură de stil de nivel sintactic care constă în alăturarea unui cuvânt calificativ la altul, pentru a obține o valoare estetică. \n (c)Metafora este o figură de stil de nivel sintactic în care doi termeni, aflați în relație de asemănare, sunt puși în paralel, pentru a scoate în evidență caracteristicile unuia dintre ei.",
+        answer: "a"
+    },
+    {
+        prompt:"Romanul 'Ion' scris de Liviu Rebreanu este un roman: \n (a) traditional \n (b)modern \n (c)balzacian",
+        answer: "b"
+
+    },
+    {
+        prompt: "Care era locul lui Ilie Moromete la masa?\n (a) Locul lui era pragul celei de-a doua odai\n (b) Locul lui era langa soba \n(c) Locul lui era langa usa ",
+        answer: "a"
+    },
+    {
+        prompt:"Cine a scris opera Plumb:\n(a) Camil Petrescu \n(b) Ion Creanga \n(c) George Bacovia",
+        answer: "c"
+
+    },
+    {
+        prompt:"Cum il caracterizeaza pe Lica Samadaul:\n(a) ca pe un prieten pentru Ghita \n(b) ca pe un stapan absolut peste oameni si locuri \n(c) ca pe un om de rand",
+        answer: "b"
+
+    },
+    {
+        prompt:":\n(a) \n(b) \n(c) \n(d)",
+        answer: ""
+
+    },
+    {
+        prompt:":\n(a) \n(b) \n(c) \n(d)",
+        answer: ""
+
+    },
+]
+var nr_intrebari=0;
